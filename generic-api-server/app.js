eab2e07149b5e57b9ff16861ee8896d4a8a8f0ca
Govindaraj Rangan <govindaraj.rangan@wipro.com>
2018-09-12 19:36:55 -0400

    Added resources

--- generic-api-server/app.js
+++ generic-api-server/app.js
@@ -1,9 +1,10 @@
 //FileName: app.js
 var express = require('express');
-var questions = require("/opt/routes/common.js");
+var common = require("/opt/routes/common.js");
 
 var app = express();
 app.get('/', common.getDefaultResponse);
+app.get('*', common.getResource);
 
 var http = require('http').Server(app);
 http.listen(3000, function(){
--- generic-api-server/routes/common.js
+++ generic-api-server/routes/common.js
@@ -1,9 +1,33 @@
+var fs = require('fs'),
+    path = require('path');
+
 exports = module.exports = {
     getDefaultResponse: function (req, res)
     {
+        //var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        //console.log("Connection from " + ip)
         var apiUri = req.headers.host + '/<resource>';
         res.writeHead(200, {"Content-Type": "application/json"});
         res.write('{"r":"API Server is healthy. Access the api using ' + apiUri + '"}');
+        //res.write('{"r":"API Server is healthy"}');
         res.end();
+    },
+    getResource: function (req, res)
+    {
+        var resourcePath = req.url;
+        var basePath = path.dirname(fs.realpathSync(__filename)) + '/../resources';
+        var filePath = basePath + resourcePath + '.json';
+        console.log(filePath);
+        if (fs.existsSync(filePath)) {
+            var resourceJson = fs.readFileSync(filePath);
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(resourceJson);
+            res.end();            
+        }
+        else {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.write('{"r":"Resource ' + resourcePath + ' unavailable"}');
+            res.end();
+        }
     }
 }
